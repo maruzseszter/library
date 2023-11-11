1b2e9b0999875809acb1d0fa5df8e16732fbd0a3
maruzseszter <eszter.maruzs@gmail.com>
2023-11-11 22:38:44 +0100

    BooksContainer begin

--- Client/src/components/BooksContainer.jsx
+++ Client/src/components/BooksContainer.jsx
@@ -1,4 +1,27 @@
+import Book from './Book';
+import Wrapper from '../assets/wrappers/BooksContainer';
+import { useAllBooksContext } from '../pages/AllBooks';
+
 const BooksContainer = () => {
-    return <h2>BooksContainer</h2>;
+  const { data } = useAllBooksContext();
+  const { books } = data;
+  if (books.length === 0) {
+    return (
+      <Wrapper>
+        <h2>Nincsenek megjeleníthető könyvek....</h2>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <div className='books'>
+        {books.map((book) => {
+          return <Book key={book._id} {...book} />;
+        })}
+      </div>
+    </Wrapper>
+  );
 };
+
 export default BooksContainer;
