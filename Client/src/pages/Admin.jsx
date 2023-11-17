388d8d5a520eb6d997168a7fcf292e7c88bbf1d7
maruzseszter <eszter.maruzs@gmail.com>
2023-11-17 21:44:51 +0100

    make Admin

--- Client/src/pages/Admin.jsx
+++ Client/src/pages/Admin.jsx
@@ -1,7 +1,27 @@
+import { FaSuitcaseRolling, FaCalendarCheck } from 'react-icons/fa';
+
+import { useLoaderData, redirect } from 'react-router-dom';
+import customFetch from '../utils/customFetch';
+import Wrapper from '../assets/wrappers/StatsContainer';
+import { toast } from 'react-toastify';
+
+export const loader = async () => {
+  try {
+    const response = await customFetch.get('/users/admin/app-stats');
+    return response.data;
+  } catch (error) {
+    toast.error('Nincs jogosultságod az oldal megtekintéséhez');
+    return redirect('/dashboard');
+  }
+};
 
 const Admin = () => {
-  return (
-    <h1>Admin Page</h1>
-  )};
+  const { users, books } = useLoaderData();
 
+  return (
+    <Wrapper>
+      <h2>admin page</h2>
+    </Wrapper>
+  );
+};
 export default Admin;
