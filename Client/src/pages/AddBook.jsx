75d7c7ac11d4f2179bbfc42296648fa59ca9503f
maruzseszter <eszter.maruzs@gmail.com>
2023-11-17 12:02:17 +0100

    English version

--- Client/src/pages/AddBook.jsx
+++ Client/src/pages/AddBook.jsx
@@ -1,7 +1,6 @@
 import { FormRow, FormRowSelect } from '../components';
 import Wrapper from '../assets/wrappers/DashboardFormPage';
 import { useOutletContext } from 'react-router-dom';
-//import { BOOK_STATUS, BOOK_TYPE } from '../../../utils/constants';
 import { BOOK_TYPE } from '../../../utils/constants';
 import { Form, useNavigation, redirect } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -29,23 +28,17 @@ const AddBook = () => {
       <Form method='post' className='form'>
         <h4 className='form-title'>Könyv hozzáadása</h4>
         <div className='form-center'>
-          <FormRow type='text' name='cím' />
-          <FormRow type='text' name='szerző' />
+          <FormRow type='text' name='title' labelText='cím'/>
+          <FormRow type='text' name='author' labelText='szerző' />
           <FormRow type='number' name='ISBN' />
-          <FormRow type='text' name='kiadó' />
-          <FormRow type='number' name='év' />
+          <FormRow type='text' name='publisher' labelText='kiadó'/>
+          <FormRow type='number' name='year' labelText='év'/>
           <FormRowSelect
             labelText='kategória'
-            name='kategória'
+            name='bookType'
             list={Object.values(BOOK_TYPE)}
           />
-          <FormRow type='number' name='darabszám' />
-          {/* <FormRowSelect
-            labelText='book status'
-            name='bookStatus'
-            defaultValue={BOOK_STATUS.NEW}
-            list={Object.values(BOOK_STATUS)}
-          /> */}
+          <FormRow type='number' name='quantity' labelText='darabszám'/>
 
           <button
             type='submit'
