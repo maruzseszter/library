7643c57543090dc0582c8cac645a05174bd38ae7
maruzseszter <eszter.maruzs@gmail.com>
2023-11-21 16:25:59 +0100

    Kilépés

--- Client/src/components/LogoutContainer.jsx
+++ Client/src/components/LogoutContainer.jsx
@@ -20,7 +20,7 @@ const LogoutContainer = () => {
       </button>
       <div className={showLogout ? 'dropdown show-dropdown' : 'dropdown'}>
         <button type='button' className='dropdown-btn' onClick={logoutUser}>
-          logout
+          Kilépés
         </button>
       </div>
     </Wrapper>
