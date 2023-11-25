e1679bc67add053e88bd2b56dd80859435b9193f
respat <respergerpatrik@gmail.com>
2023-11-25 10:18:31 +0100

    Hot reload fix, fájl feltöltés előkészítése.

--- Client/src/components/Users.jsx
+++ Client/src/components/Users.jsx
@@ -3,7 +3,7 @@ import UserImport from "./UserImport";
 const Users = () => {
   return (
     <>
-        <h2>Felhasználók</h2>
+        <h2>Felhasználók kezelése</h2>
         <UserImport/>
     </>
   );
